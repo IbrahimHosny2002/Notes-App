1ec7cb83bb5d010cdee04ec320e7cfb28c344195
IbrahimHosny2002 <hosnyhema66@gmail.com>
2024-02-27 23:02:18 +0200

    create note view body and custom app bar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24
+      ),
+      child: Column(
+        children:  [
+          SizedBox(
+            height: 35,
+          ),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+
+
